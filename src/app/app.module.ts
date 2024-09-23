6e63b81099582e210ba17a0425a0239ca29a77b3
CesarBaquiro <cmbm.kf@gmail.com>
2024-09-22 21:12:51 -0500

    Change proyect structure

---
+++ src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [];
+
+@NgModule({
+  declarations: [],
+  imports: [BrowserModule, AppRoutingModule, RouterModule.forRoot(appRoutes)],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
