1dcc3f4a0dfaa4b3c3373e34084a2f9b6790cff6
CesarBaquiro <cmbm.kf@gmail.com>
2024-11-16 07:26:18 -0500

    Add roulette prohibited

---
+++ src/app/components/roulettes-menu/roulettes-menu.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-roulettes-menu',
+  templateUrl: './roulettes-menu.component.html',
+  styleUrls: ['./roulettes-menu.component.css']
+})
+export class RoulettesMenuComponent {
+
+}
